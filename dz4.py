739da816264d33fcc2e49d393214981ec217a57d
Demid <balkovoydemid2006.2@gmail.com>
2025-10-29 16:55:58 +0700

    -

---
+++ dz4.py
@@ -0,0 +1,10 @@
+n = int(input("Введите N: "))
+
+for num in range(2, n + 1):
+    is_prime = True
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num)
